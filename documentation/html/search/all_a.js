64cf605bf3ac9c189bcb38ddbe6faec0d7ff1cc0
Manuel Reinfurt <manuel.reinfurt@outlook.com>
2014-01-10 21:47:45 +0100

    Improved doxygen HTML output

--- documentation/html/search/all_a.js
+++ documentation/html/search/all_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['voozix',['Voozix',['../index.html',1,'']]],
   ['viewcontroller',['ViewController',['../interface_view_controller.html',1,'']]]
 ];
--- documentation/html/search/search.js
+++ documentation/html/search/search.js
@@ -9,14 +9,16 @@ var indexSectionsWithContent =
 {
   0: "acehimprstv",
   1: "acehmpsv",
-  2: "cirt"
+  2: "cirt",
+  3: "v"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "classes",
-  2: "functions"
+  2: "functions",
+  3: "pages"
 };
 
 function convertToId(search)
