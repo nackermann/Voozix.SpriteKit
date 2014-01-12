30a853db6b2471bfe1cdc76bd4cd97a7943c15b4
Manuel Reinfurt <manuel.reinfurt@outlook.com>
2014-01-12 13:24:26 +0100

    Removed documentation from master

--- documentation/html/search/all_a.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['score',['score',['../interface_player.html#a80990c90b90acf7a5cae7f4858cbc129',1,'Player']]],
-  ['soundmanager',['SoundManager',['../interface_sound_manager.html',1,'']]],
-  ['star',['Star',['../interface_star.html',1,'']]]
-];
